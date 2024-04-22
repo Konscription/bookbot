aac6d3a8b2ce85ec0cfe6737a9b84de60560ea6c
Konscription <konviction@gmail.com>
2024-04-22 13:02:34 -0400

    added a character counter function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
     print(f"{word_count} words found in the book.")
+    print(get_letter_count(book_text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -14,7 +15,16 @@ def get_word_count(text):
 #Count Letters
 def get_letter_count(text):
     lowered_string = text.lower()
-    
+    unique_characters = ''.join(set(lowered_string))
+    character_counts = {}
+    for char in unique_characters:
+        char_count = lowered_string.count(char)
+        character_counts[char] = char_count
+    return character_counts
+
+def log(text,is_debug):
+    if is_debug:
+        print(text)
 
 main()
     
