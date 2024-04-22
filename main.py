c3bff3989a7bbf705451cd3269edfa515014a054
Konscription <konviction@gmail.com>
2024-04-22 13:25:48 -0400

    created report function to replace disprate print calls

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the book.")
-    print(get_letter_count(book_text))
+    count_by_letter = get_letter_count(book_text)
+    print_report(book_path,word_count,count_by_letter)
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,19 +12,34 @@ def get_book_text(path):
 def get_word_count(text):
     return len(text.split())
 
+def sort_on(dict):
+    return dict["count"]
+#sort dict
+def sort_dict(list):
+    list.sort(reverse=True, key=sort_on)
+    return list
+
 #Count Letters
 def get_letter_count(text):
     lowered_string = text.lower()
     unique_characters = ''.join(set(lowered_string))
-    character_counts = {}
+    character_counts_sortable = []
     for char in unique_characters:
-        char_count = lowered_string.count(char)
-        character_counts[char] = char_count
-    return character_counts
+        if char.isalpha():
+            character_counts= {}
+            char_count = lowered_string.count(char)
+            character_counts["char"] = char
+            character_counts["count"] = char_count
+            character_counts_sortable.append(character_counts)
+    return sort_dict(character_counts_sortable)
 
-def log(text,is_debug):
-    if is_debug:
-        print(text)
+
+def print_report(book_path,word_count_total,count_by_letter):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count_total} words found in the book.\n")
+    for entry in count_by_letter:
+        print(f"The '{entry["char"]}' character was found {entry["count"]} times")
+    print("--- End report ---")
 
 main()
     
