18dee36fdc6d9f0a34290d26ef1a6c8b37725fb1
Konscription <konviction@gmail.com>
2025-04-30 10:01:55 -0400

    update for lesson changes
    updated with new lesson changes

--- main.py
+++ main.py
@@ -1,45 +1,55 @@
+import sys
+
+from stats import get_num_words
+
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    word_count = get_word_count(book_text)
-    count_by_letter = get_letter_count(book_text)
-    print_report(book_path,word_count,count_by_letter)
+    if len(sys.argv) == 1:
+        print("Usage: python main.py <book_path>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_word_count(text):
-    return len(text.split())
-
-def sort_on(dict):
-    return dict["count"]
-#sort dict
-def sort_dict(list):
-    list.sort(reverse=True, key=sort_on)
-    return list
-
-#Count Letters
-def get_letter_count(text):
-    lowered_string = text.lower()
-    unique_characters = ''.join(set(lowered_string))
-    character_counts_sortable = []
-    for char in unique_characters:
-        if char.isalpha():
-            character_counts= {}
-            char_count = lowered_string.count(char)
-            character_counts["char"] = char
-            character_counts["count"] = char_count
-            character_counts_sortable.append(character_counts)
-    return sort_dict(character_counts_sortable)
-
-
-def print_report(book_path,word_count_total,count_by_letter):
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count_total} words found in the book.\n")
-    for entry in count_by_letter:
-        print(f"The '{entry["char"]}' character was found {entry["count"]} times")
-    print("--- End report ---")
 
 main()
-    
